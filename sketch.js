7e30cd582ba2c259e28e3900ce04ec70cd7e2aa2
Vino1332 <123568049+Vino1332@users.noreply.github.com>
2023-03-06 10:52:29 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let sunrise = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(60, 219, 240);
+ fill(82, 46, 20);
+  rect(0,300,500)
+  
+  fill(245, 231, 81);
+  ellipse (200,sunrise+350,200);
+  sunrise=sunrise-1;
+  
+    fill(60, 240, 66)
+ ellipse(200,150,50);
+  rect(175,180,50,200);
+  rect(180,150,10);
+  rect(210,150,10);
+rect(25,200,150,20 );
+rect(225,200,150,20);
+
+  
+
+
+
+
+} 
